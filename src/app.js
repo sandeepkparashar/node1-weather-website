840501a0d2b864f4d9c22ed92d1a9c355c2dd616
Sandeep Parashar <sandeep.parashar@castingnetworks.com>
2020-05-25 10:02:13 +0530

    Init Commit

---
+++ src/app.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+const app = express();
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+app.set("views", viewPath);
+app.set("view engine", "hbs");
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", { title: "Weather", name: "Sandeep Parashar" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Us Page",
+    name: "Sndeep Kumar",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    name: "Sandeep Parashar",
+    helpText: "Please post your queries here.",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({ error: "Please supply address" });
+  }
+  const address = req.query.address;
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+      res.send({
+        location,
+        forecastData,
+      });
+    });
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    errorMessage: "Help article not found.",
+    title: "Help 404 page",
+    name: "Sandeep",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    errorMessage: "Page not found.",
+    title: "404 Page",
+    name: "Sandeep",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server is up on port 3000");
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,30 @@
+const request = require("postman-request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=4eea257d78016bd0ababd05fd499bf12&query=" +
+    latitude +
+    "," +
+    longitude;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          "." +
+          " It is currently " +
+          body.current.temperature +
+          " degress out. It feels like " +
+          body.current.feelslike +
+          "."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
